f409098ebb66a4fe382170ca1571002d47da931e
Mike L <mike@desertrat.io>
2021-03-07 20:56:19 -0600

    tests and shit

--- app/controllers/api/v1/base_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Api
-  module V1
-    class BaseController < ActionController::API
-      respond_to :json
-    end
-  end
-end
---
+++ spec/helpers/request_spec_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module RequestSpecHelper
+  def json
+    JSON.parse(response.body)
+  end
+end
